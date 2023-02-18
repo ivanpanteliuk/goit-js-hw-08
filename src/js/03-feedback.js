b68f25f5f966cc46e64192e61f4accdc30c7d45a
Ivan Panteliuk <i.v.pantelyuk@gmail.com>
2023-02-18 22:46:05 +0100

    Fixing feedback form bugs

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,7 +7,6 @@ const refs = {
 };
 const KEY = 'feedback-form-state';
 const formData = { email: '', message: '' };
-const parsedFormData = JSON.parse(localStorage.getItem(KEY)) || {};
 
 refs.formEl.addEventListener('input', throttle(onInput, 500));
 refs.formEl.addEventListener('submit', onSubmit);
@@ -15,23 +14,21 @@ refs.formEl.addEventListener('submit', onSubmit);
 populateForm();
 
 function onInput(e) {
-  formData.email = refs.emailEl.value;
-  formData.message = refs.textareaEl.value;
-  localStorage.setItem(KEY, JSON.stringify(formData));
+  formData.email = refs.emailEl.value.trim();
+  formData.message = refs.textareaEl.value.trim();
+  return localStorage.setItem(KEY, JSON.stringify(formData));
 }
 
 function populateForm() {
-  if (parsedFormData.email) {
-    refs.emailEl.value = parsedFormData.email;
-  }
-  if (parsedFormData.message) {
-    refs.textareaEl.value = parsedFormData.message;
-  }
+  const parsedFormData = JSON.parse(localStorage.getItem(KEY)) || {};
+  refs.emailEl.value = parsedFormData.email || '';
+  refs.textareaEl.value = parsedFormData.message || '';
+  return parsedFormData;
 }
 
 function onSubmit(e) {
   e.preventDefault();
-  console.log(formData);
+  console.log(populateForm());
   refs.formEl.reset();
   localStorage.removeItem(KEY);
 }
